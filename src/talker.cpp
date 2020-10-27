9d8fa08bf288452ff74a8cb42549953c346f39b1
Ajinkya Parwekar <ajinkyaparwekar9@gmail.com>
2020-10-27 16:46:27 -0400

    modified file with custom string message

--- src/talker.cpp
+++ src/talker.cpp
@@ -60,7 +60,7 @@ int main(int argc, char **argv) {
     std_msgs::String msg;
 
     std::stringstream ss;
-    ss << "hello world " << count;
+    ss << "I'm learning ROS " << count;
     msg.data = ss.str();
 
     ROS_INFO("%s", msg.data.c_str());
